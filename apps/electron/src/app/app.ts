1b339bbd395691e7ba95b5483f8b152e4842c93d
luke.wu <luke.wu@nokia-sbell.com>
2021-03-03 11:07:44 +0800

    refactor: resize app window

--- apps/electron/src/app/app.ts
+++ apps/electron/src/app/app.ts
@@ -107,8 +107,8 @@ export default class App {
 
   private static initMainWindow() {
     const workAreaSize = screen.getPrimaryDisplay().workAreaSize;
-    const width = Math.min(1280, workAreaSize.width || 1280);
-    const height = Math.min(720, workAreaSize.height || 720);
+    const width = Math.min(900, workAreaSize.width || 900);
+    const height = App.application.isPackaged? Math.min(540, workAreaSize.height || 540) : Math.min(720, workAreaSize.height || 720);
 
     // Create the browser window.
     App.mainWindow = new BrowserWindow({
