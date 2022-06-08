92a0d343933a73a30ecced63ec7bfdb9d5cbf1e8
daihere1993 <daihere1993@gmail.com>
2022-06-08 09:42:09 +0800

    chore: update GH_TOKEN

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 export http_proxy=http://10.158.100.3:8080
 export https_proxy=http://10.158.100.3:8080
-export GH_TOKEN=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
+export GH_TOKEN=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
 yarn run build --prod && nx run electron:make --platform windows --arch x64 --publishPolicy always
