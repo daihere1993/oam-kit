8950be757a495e620f4a1be6da08adc84693c8f6
daihere1993 <daihere1993@gmail.com>
2022-07-02 23:34:54 +0800

    chore: upgrade to v0.3.11

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 export http_proxy=http://10.158.100.3:8080
 export https_proxy=http://10.158.100.3:8080
-export GH_TOKEN=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
+export GH_TOKEN=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
 yarn run build --prod && nx run electron:make --platform windows --arch x64 --publishPolicy always
