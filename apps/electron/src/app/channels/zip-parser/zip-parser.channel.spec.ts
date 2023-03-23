d38c112e3d0e17b8b58959947e00822d440ac609
daihere1993 <daihere1993@gmail.com>
2023-03-23 09:14:52 +0800

    fix: windows ut adaption

--- apps/electron/src/app/channels/zip-parser/zip-parser.channel.spec.ts
+++ apps/electron/src/app/channels/zip-parser/zip-parser.channel.spec.ts
@@ -6,11 +6,24 @@ import { ZipParserChannel } from './zip-parser.channel';
 
 const rmFiles = util.promisify(fs.rm);
 
+function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function cleanupFiles() {
+  await timeout(1000);
+  await rmFiles(path.join(__dirname, 'test-resources/snapshot'), { recursive: true, force: true });
+}
+
 describe('ZipParser: private method', () => {
   const zipParser = new ZipParserChannel(null);
 
   describe('_unzipByRules():', () => {
     const src = path.join(__dirname, 'test-resources/snapshot.zip');
+
+    afterEach(async () => {
+      await cleanupFiles();
+    })
 
     it('should get all ims2 files correctlly', async () => {
       const rules: Rule[] = [{ name: 'ims2', firstRegex: /.+\.ims2/, parsingInfos: { pathList: [] } }];
@@ -26,8 +39,6 @@ describe('ZipParser: private method', () => {
       expect(ims2Rule.parsingInfos.pathList).toContain(
         'MRBTS-1900_TL SG7 129 AEQC_5G21A_GNB_0011_000800_000282_SMOKE38_20210312-1755.ims2'
       );
-
-      await rmFiles(path.join(__dirname, 'test-resources/snapshot'), { recursive: true, force: true });
     }, 30000);
     it('should get all soap files correctlly', async () => {
       const rules: Rule[] = [{ name: 'soap messages', firstRegex: /.+SOAPMessageTrace.+/, parsingInfos: { pathList: [] } }];
@@ -41,8 +52,6 @@ describe('ZipParser: private method', () => {
         .toContain('BTS1900_1011_part_10/BTS1900_1011_L1183908869_RMOD_L_1_SOAPMessageTrace.xml');
       expect(soapRule.parsingInfos.pathList)
         .toContain('BTS1900_1011_part_10/BTS1900_1011_L1183908869_RMOD_L_1_SOAPMessageTrace_old.xml');
-
-      await rmFiles(path.join(__dirname, 'test-resources/snapshot'), { recursive: true, force: true });
     }, 30000);
     it('should get all moam_runtime(1011).log.xz files correctlly', async () => {
       const rules: Rule[] = [
@@ -61,8 +70,6 @@ describe('ZipParser: private method', () => {
       expect(moamLogRule.parsingInfos.pathList.length).toBe(1);
       expect(moamLogRule.parsingInfos.pathList)
         .toContain('BTS1900_1011_part_3/BTS1900_1011_runtime/runtime_BTSOM.log.xz');
-      
-      await rmFiles(path.join(__dirname, 'test-resources/snapshot'), { recursive: true, force: true });
     }, 30000);
     it('should get all moam_runtime(2011).log.xz files correctlly', () => {});
     it('should get all moam_runtime(1011).log files correctlly when file has been decompressed', () => {});
